a973a09f26e4981baaff26c5824dae2325a9341f
joaquind <joaquind@students.21-school.ru>
2023-10-28 19:18:04 +0300

    Write basic handler

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -3,13 +3,46 @@
 //
 
 #include "client.h"
-#include "asio.hpp"
+#include <termios.h>
 
 namespace joaquind {
     void Client::Connect() {
-        asio::io_context io;
-        asio::ip::tcp::endpoint ep(asio::ip::address::from_string("127.0.0.1"), 8080);
-        asio::ip::tcp::socket s(io);
-        s.connect(ep);
+        TurnOffBufferingInput();
+        s_.async_connect(ep_, [this](const asio::error_code &e) { if (!e) Session(); });
+        io_.run();
     }
+
+    void Client::TurnOffBufferingInput() {
+        struct termios settings{};
+        tcgetattr(STDIN_FILENO, &settings);
+        settings.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &settings);
+    }
+
+    void Client::Session() {
+        ReadFromConsole();
+        ReadFromSocket();
+    }
+
+    void Client::WriteToSocket() {
+        s_.async_write_some(asio::buffer(symbol_, 1),
+                            [this](const asio::error_code &e, std::size_t) { if (!e) ReadFromConsole(); });
+    }
+
+    void Client::ReadFromConsole() {
+        input_.async_read_some(asio::buffer(symbol_, 1),
+                               [this](const asio::error_code &e, std::size_t) { if (!e) WriteToSocket(); });
+    }
+
+    void Client::ReadFromSocket() {
+        auto str = std::make_shared<std::string>();
+        s_.async_read_some(asio::buffer(*str), [this, str](const asio::error_code &e, std::size_t bytes){ if (!e) PrintField(str); });
+    }
+
+    void Client::PrintField(const std::shared_ptr<std::string>& s_ptr) {
+        std::cout << *s_ptr << '\n';
+        ReadFromSocket();
+    }
+
+
 } // joaquind
